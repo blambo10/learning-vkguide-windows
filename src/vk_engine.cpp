7a8711351f3eb108cf476100c381aa8289ec2c29
blambo10 <blamb@live.com.au>
2026-07-07 18:31:08 +1000

    wip

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -1,5 +1,5 @@
 ﻿//Continue here https://vkguide.dev/docs/new_chapter_5/gltf_nodes/
-// in vk_load.cpp line 301 "Now we can load the samplers from the glTF file."
+// in vk_load.cpp line 502 "The difference is that at the end, we handle the material index. If there is no material, we will default on"
 
 //Note: to modify the monkey head transparency, update the vec4 opactiry field in the fragment shader at coloured_triangle.frag, then rerun the shader compile..bat
 
--- src/vk_loader.cpp
+++ src/vk_loader.cpp
@@ -251,6 +251,38 @@ std::optional<std::vector<std::shared_ptr<MeshAsset>>> loadGltfMeshes(VulkanEngi
 	return meshes;
 }
 
+VkFilter extract_filter(fastgltf::Filter filter) {
+	switch (filter) {
+		//Nearest samples
+	case fastgltf::Filter::Nearest:
+	case fastgltf::Filter::NearestMipMapNearest:
+	case fastgltf::Filter::NearestMipMapLinear:
+		return VK_FILTER_NEAREST;
+
+		//Linear samples
+	case fastgltf::Filter::Linear:
+	case fastgltf::Filter::LinearMipMapNearest:
+	case fastgltf::Filter::LinearMipMapLinear:
+	default:
+		return VK_FILTER_LINEAR;
+
+
+	}
+}
+
+VkSamplerMipmapMode extract_mipmap_mode(fastgltf::Filter filter) {
+	switch (filter) {
+	case fastgltf::Filter::NearestMipMapNearest:
+	case fastgltf::Filter::LinearMipMapNearest:
+		return VK_SAMPLER_MIPMAP_MODE_NEAREST;
+
+	case fastgltf::Filter::NearestMipMapLinear:
+	case fastgltf::Filter::LinearMipMapLinear:
+	default:
+		return VK_SAMPLER_MIPMAP_MODE_LINEAR;
+	}
+}
+
 std::optional<std::shared_ptr<LoadedGLTF>> loadGltf(VulkanEngine* engine, 
 													std::filesystem::path filePath) {
 	fmt::print("LoadingGLTF: {}", filePath);
@@ -300,36 +332,172 @@ std::optional<std::shared_ptr<LoadedGLTF>> loadGltf(VulkanEngine* engine,
 
 	//TODO: continue loading samples here with new functions below
 	//"Now we can load the samplers from the glTF file."
-}
 
-VkFilter extract_filter(fastgltf::Filter filter) {
-	switch (filter) {
-		//Nearest samples
-		case fastgltf::Filter::Nearest:
-		case fastgltf::Filter::NearestMipMapNearest:
-		case fastgltf::Filter::NearestMipMapLinear:
-			return VK_FILTER_NEAREST;
+	for (fastgltf::Sampler& sampler : gltf.samplers) {
+			VkSamplerCreateInfo sampl = { .sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO, .pNext = nullptr };
+			sampl.maxLod = VK_LOD_CLAMP_NONE;
+			sampl.minLod = 0;
 
-		//Linear samples
-		case fastgltf::Filter::Linear:
-		case fastgltf::Filter::LinearMipMapNearest:
-		case fastgltf::Filter::LinearMipMapLinear:
-		default:
-			return VK_FILTER_LINEAR;
+			sampl.magFilter = extract_filter(sampler.magFilter.value_or(fastgltf::Filter::Nearest));
+			sampl.minFilter = extract_filter(sampler.minFilter.value_or(fastgltf::Filter::Nearest));
 
+			sampl.mipmapMode = extract_mipmap_mode(sampler.minFilter.value_or(fastgltf::Filter::Nearest));
 
+			VkSampler newSampler;
+			vkCreateSampler(engine->_device, &sampl, nullptr, &newSampler);
+
+			file.samplers.push_back(newSampler);
+	};
+
+	std::vector<std::shared_ptr<MeshAsset>> meshes;
+	std::vector<std::shared_ptr<Node>> nodes;
+	std::vector<AllocatedImage> images;
+	std::vector<std::shared_ptr<GLTFMaterial>> materials;
+
+	//load all textures
+	for (fastgltf::Image& image : gltf.images) {
+		images.push_back(engine->_errorCheckerboardImage);
+	}
+
+	//create buffer to hold the material data
+	file.materialDataBuffer = engine->create_buffer(sizeof(GLTFMetallic_Roughness::MaterialConstants) * gltf.materials.size(),
+		VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, VMA_MEMORY_USAGE_CPU_TO_GPU);
+	int data_index = 0;
+	GLTFMetallic_Roughness::MaterialConstants* sceneMaterialConstants = (GLTFMetallic_Roughness::MaterialConstants*)file.materialDataBuffer.info.pMappedData;
+
+	for (fastgltf::Material& mat : gltf.materials) {
+		std::shared_ptr<GLTFMaterial> newMat = std::make_shared<GLTFMaterial>();
+		materials.push_back(newMat);
+		file.materials[mat.name.c_str()] = newMat;
+
+		GLTFMetallic_Roughness::MaterialConstants constants;
+		constants.colorFactors.x = mat.pbrData.baseColorFactor[0];
+		constants.colorFactors.y = mat.pbrData.baseColorFactor[1];
+		constants.colorFactors.z = mat.pbrData.baseColorFactor[2];
+		constants.colorFactors.w = mat.pbrData.baseColorFactor[3];
+
+		constants.metal_rough_factors.x = mat.pbrData.metallicFactor;
+		constants.metal_rough_factors.y = mat.pbrData.roughnessFactor;
+
+		//write material params to buffer
+		sceneMaterialConstants[data_index] = constants;
+
+		MaterialPass passType = MaterialPass::MainColor;
+		if (mat.alphaMode == fastgltf::AlphaMode::Blend) {
+			passType = MaterialPass::Transparent;
+		}
+
+		GLTFMetallic_Roughness::MaterialResources materialResources;
+		materialResources.colorImage = engine->_whiteImage;
+		materialResources.colorSampler = engine->_defaultSamplerLinear;
+		materialResources.metalRoughImage = engine->_whiteImage;
+		materialResources.metalRoughSampler = engine->_defaultSamplerLinear;
+
+		materialResources.dataBuffer = file.materialDataBuffer.buffer;
+		materialResources.dataBufferOffset = data_index * sizeof(GLTFMetallic_Roughness::MaterialConstants);
+
+		// get textures from file
+		if (mat.pbrData.baseColorTexture.has_value()) {
+			size_t img = gltf.textures[mat.pbrData.baseColorTexture.value().textureIndex].imageIndex.value();
+			size_t sampler = gltf.textures[mat.pbrData.baseColorTexture.value().textureIndex].samplerIndex.value();
+
+			materialResources.colorImage = images[img];
+			materialResources.colorSampler = file.samplers[sampler];
+		}
+
+		newMat->data = engine->metalRoughMaterial.write_material(engine->_device, passType, materialResources, file.descriptorPool);
+
+		data_index++;
+	}
+
+	std::vector<uint32_t> indices;
+	std::vector<Vertex> vertices;
+
+	for (fastgltf::Mesh& mesh : gltf.meshes) {
+		std::shared_ptr<MeshAsset> newmesh = std::make_shared<MeshAsset>();
+		meshes.push_back(newmesh);
+		file.meshes[mesh.name.c_str()] = newmesh;
+
+		//clear the mesh arrays each mesh
+		indices.clear();
+		vertices.clear();
+
+		for (auto&& p : mesh.primitives) {
+			GeoSurface newSurface;
+			newSurface.startIndex = (uint32_t)indices.size();
+			newSurface.count = (uint32_t)gltf.accessors[p.indicesAccessor.value()].count;
+
+			size_t initial_vtx = vertices.size();
+
+			{
+				fastgltf::Accessor& indexaccessor = gltf.accessors[p.indicesAccessor.value()];
+				indices.reserve(indices.size() + indexaccessor.count);
+
+				fastgltf::iterateAccessor<std::uint32_t>(gltf, indexaccessor,
+					[&](std::uint32_t idx) {
+						indices.push_back(idx + initial_vtx);
+					});
+			}
+
+			// load vertex positions
+			{
+				fastgltf::Accessor& posAccessor = gltf.accessors[p.findAttribute("POSITION")->second];
+				vertices.resize(vertices.size() + posAccessor.count);
+
+				fastgltf::iterateAccessorWithIndex<glm::vec3>(gltf, posAccessor,
+					[&](glm::vec3 v, size_t index) {
+						Vertex newvtx;
+						newvtx.position = v;
+						newvtx.normal = { 1, 0, 0 };
+						newvtx.color = glm::vec4{ 1.f };
+						newvtx.uv_x = 0;
+						newvtx.uv_y = 0;
+						vertices[initial_vtx + index] = newvtx;
+					});
+			}
+
+			// load vertex normals
+			auto normals = p.findAttribute("NORMAL");
+			if (normals != p.attributes.end()) {
+
+				fastgltf::iterateAccessorWithIndex<glm::vec3>(gltf, gltf.accessors[(*normals).second],
+					[&](glm::vec3 v, size_t index) {
+						vertices[initial_vtx + index].normal = v;
+					});
+			}
+
+			// load UVs
+			auto uv = p.findAttribute("TEXCOORD_0");
+			if (uv != p.attributes.end()) {
+
+				fastgltf::iterateAccessorWithIndex<glm::vec2>(gltf, gltf.accessors[(*uv).second],
+					[&](glm::vec2 v, size_t index) {
+						vertices[initial_vtx + index].uv_x = v.x;
+						vertices[initial_vtx + index].uv_y = v.y;
+					});
+			}
+
+			// load vertex colors
+			auto colors = p.findAttribute("COLOR_0");
+			if (colors != p.attributes.end()) {
+
+				fastgltf::iterateAccessorWithIndex<glm::vec4>(gltf, gltf.accessors[(*colors).second],
+					[&](glm::vec4 v, size_t index) {
+						vertices[initial_vtx + index].color = v;
+					});
+			}
+
+			if (p.materialIndex.has_value()) {
+				newSurface.material = materials[p.materialIndex.value()];
+			}
+			else {
+				newSurface.material = materials[0];
+			}
+
+			newmesh->surfaces.push_back(newSurface);
+		}
+
+		newmesh->meshBuffers = engine->uploadMesh(indices, vertices);
 	}
 }
 
-VkSamplerMipmapMode extract_mipmap_mode(fastgltf::Filter filter) {
-	switch (filter) {
-		case fastgltf::Filter::NearestMipMapNearest:
-		case fastgltf::Filter::LinearMipMapNearest:
-			return VK_SAMPLER_MIPMAP_MODE_NEAREST;
-
-		case fastgltf::Filter::NearestMipMapLinear:
-		case fastgltf::Filter::LinearMipMapLinear:
-		default:
-			return VK_SAMPLER_MIPMAP_MODE_LINEAR;
-	}	
-}
